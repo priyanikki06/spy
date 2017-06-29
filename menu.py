1530dab1745b27ac5e46adac4a704786ebef4ac1
priyanikki06 <priyanikki06@gmail.com>
2017-06-29 12:30:32 +0530

    this is my first project

---
+++ menu.py
@@ -0,0 +1,11 @@
+
+
+class Spy:
+
+
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
